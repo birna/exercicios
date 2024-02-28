5fc237291ae6f8fb5b24100728e13953196c69f3
Guilherme Urbinatti <guilherme.urbinatti@hotmail.com>
2024-02-28 11:27:10 -0300

    corrigindo log

--- js/logica/equacao_segundo_grau.js
+++ js/logica/equacao_segundo_grau.js
@@ -15,7 +15,7 @@ function equacaoSegundoGrau(valor1, valor2, valor3) {
     `
         Entrada_Valor1: ${valor1},
         Entrada_Valor2: ${valor2},
-        Entrada_Valor2: ${valor3}
+        Entrada_Valor3: ${valor3}
     `
 
     resultado("Vazio", null, log)
